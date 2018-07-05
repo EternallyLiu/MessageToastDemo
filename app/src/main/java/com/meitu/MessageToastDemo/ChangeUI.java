570fb3651185824175ce746c3b2e444ac79fa3ff
Liu Pei <lp2@meitu.com>
2018-07-05 18:34:45 +0800

    推送测试6

--- app/src/main/java/com/meitu/MessageToastDemo/ChangeUI.java
+++ app/src/main/java/com/meitu/MessageToastDemo/ChangeUI.java
@@ -17,8 +17,7 @@ public class ChangeUI {
         tv.setPadding(DensityUtil.dip2px(context, 35), DensityUtil.dip2px(context, 10)
                 , DensityUtil.dip2px(context, 10), DensityUtil.dip2px(context, 10));
         tv.setTextColor(ContextCompat.getColor(context, R.color.colorTextNormal));
-       System.out.println("嘿嘿嘿");
-        System.out.println("呵呵呵");
+        System.out.println("one");
         tv.setBackground(ContextCompat.getDrawable(context, res));
     }
 
